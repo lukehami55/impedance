18b3eefeda3a6ea2454ffa03af9dfe1bf90505bc
lukehami55 <94429883+lukehami55@users.noreply.github.com>
2023-01-03 15:58:42 -0600

    Update firmata.py

--- firmata.py
+++ firmata.py
@@ -1,6 +1,5 @@
-from pyfirmata import Arduino, util, INPUT
+from pyfirmata import Arduino, util
 from datetime import datetime
-from csv import writer
 import time
 
 """
@@ -15,67 +14,42 @@ def init():
     board.analog[1].enable_reporting()
     board.analog[2].enable_reporting()
     board.digital[5].write(1)
+    board.digital[6].write(1)
+    board.digital[7].write(1)
     time.sleep(3)
     print("running")
     read(board)
-    
-    
+
+
 """
 read1 gets first impedence reading
 read1 always goes high no matter the location
 """
 def read(board):
     while True:
-        read1 = board.analog[0].read() 
-        if read1 > 0.2: #
-            broke(board) #analyze location of break
+        read1 = board.analog[0].read()
+        read2 = board.analog[1].read()
+        read3 = board.analog[2].read()
+        if read1 > 0.2:
+            broke(board,read1,read2,read3,1)
+            break
+        elif read2 > 0.2:
+            broke(board,read1,read2,read3,2)
+            break
+        elif read3 > 0.2:
+            broke(board,read1,read2,read3,3)
             break
         else:
             with open("output.txt", "a") as output:
-                output.write(str(datetime.now())+","+str(read1)+","+str(read1)+","+str(read1)+"\n")
+                output.write(str(datetime.now()) + "," + str(read1) + "," + str(read2) + "," + str(read3) + "\n")
 
-                
-"""
-reference "leadFlow.png" diagram
-seperate function and large range used as it takes time for read2 and read3 to populate
-break1 = faulty bend1
-break2 = faulty bend2
-break3 = faulty bend3
-
-logic flow:
-read1 is known to be high
-if no additional high value in range interval, break1 is true
-if read2 is high in interval, turns off break1 possibility and adds break2 possibility
-if read3 is high, break can only occur at break3 ("only" is reason for "break" statement), turns off break1 and break2 possibility
-"""
-def broke(board):
-    break1 = True
-    break2 = False
-    break3 = False
-    for i in range(100000):
-        read2 = board.analog[1].read()
-        read3 = board.analog[2].read()
-        if read3 > 0.2:
-            break1 = False
-            break2 = False
-            break3 = True
-            break
-        if read2 > 0.2:
-            break1 = False
-            break2 = True
-    alert(break1, break2, break3)
-
-        
 """
 future alert sms system 
 """
-def alert(break1, break2, break3):
-    if break1:
-        print("broken first one")
-    if break2:
-        print("broken second one")
-    if break3:
-        print("broken third one")
+def broke(board,read1,read2,read3,lead):
+    print("broken lead "+str(lead))
+    with open("output.txt", "a") as output:
+        output.write(str(datetime.now()) + "," + str(read1) + "," + str(read2) + "," + str(read3) + "\n")
 
 
 if __name__ == '__main__':
